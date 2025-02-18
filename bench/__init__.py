38a31e2243717ff2aa6b2f50cb9de05936f6738c
Zlatin Balevsky <zlatinb@gmail.com>
2025-02-19 01:47:32 +0200

    raise on all numerical errors, better name and documentation o f the example benchmark config

--- bench/__init__.py
+++ bench/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 from .driver import Driver
 
-np.seterr(over='raise', divide='raise', invalid='raise', under='ignore')
+np.seterr(over='raise', divide='raise', invalid='raise', under='raise')
 
 __all__ = ["Driver"]
--- classifier/__init__.py
+++ classifier/__init__.py
@@ -4,6 +4,6 @@ from .stability import *
 from .properties import GlobalProperties
 from .forward import ForwardPass
 
-np.seterr(over='raise', divide='raise', invalid='raise', under='ignore')
+np.seterr(over='raise', divide='raise', invalid='raise', under='raise')
 __all__ = ["Stabilizer","GlobalProperties","Functions","ForwardPass"]
 
